bc1a4088f42b7120c7cdef2fe32da3b15d5aaebf
Navi-Beep <estawars30@gmail.com>
2022-11-15 16:24:50 -0600

    changed navbar text

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg bg-light">
         <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="#">Bird App</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
         </button>
