2624d579138288f73958bfa114c904922deed1e2
Navi-Beep <estawars30@gmail.com>
2022-12-01 22:52:58 -0600

    added some shadows

--- src/components/birddata.js
+++ src/components/birddata.js
@@ -7,35 +7,32 @@ let tableHeaders = ['Species','Conservation Status', 'Latin Name', 'Habitat']
 export default function birddata() {
 
     return (
-        <div className="row">
-        <table className="dflex container-fluid table table-light table-striped mt-3 w-50 text-center shadow-sm p-3 mb-5 bg-body rounded">
-        <thead>
-        <tr>
-        {tableHeaders.map((th,i) => <th key={i}>{th}</th>)}
-        </tr>
-        </thead>
-            <tbody>
-                <tr>
-                <td>Cockatiel</td>
-                <td>Least Concern</td>
-                <td>Nymphicus hollandicus</td>
-                <td>Australia</td>
-                </tr>
-                <tr>
-                <td>Blue Macaw</td>
-                <td>Extinct in the wild</td>
-                <td>Cyanopsitta spixii</td>
-                <td>Northeastern Brazil</td>
-                </tr>
-                <tr>
-                <td>Tawny Owl</td>
-                <td>Least Concern</td>
-                <td>Strix aluco</td>
-                <td>Woodlands across Europe to Siberia</td>
-                </tr>
-            </tbody>
-        </table>
-        </div>
+
+            <div className='row'>
+
+            <div id="carouselExampleControls" class="carousel slide d-flex container-fluid shadow p-3 mb-5 bg-body rounded" style={{width:'25em'}} data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="https://picsum.photos/400" class="d-block w-100 border rounded" alt="..."/>
+                </div>
+                <div class="carousel-item">
+                <img src="https://picsum.photos/400" class="d-block w-100 border rounded" alt="..."/>
+                </div>
+                <div class="carousel-item">
+                <img src="https://picsum.photos/400" class="d-block w-100 border rounded" alt="..."/>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+            </div>
+            </div>
+        
 
     );
 };
