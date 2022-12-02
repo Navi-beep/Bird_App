3f5b78148e40a901f08a04ed15b203be5be11df9
Navi-Beep <estawars30@gmail.com>
2022-12-01 18:55:04 -0600

    made changes

--- src/components/space.js
+++ src/components/space.js
@@ -24,10 +24,10 @@ export default function Spacefacts(props) {
     return (
 
         <div className='row'>
-            <div className="dflex container-fluid card" style={{width:'25em'}}>
-                <h2  className="text-center mt-2">Astronomy Photo of the day</h2>
+            <div className="dflex container-fluid card shadow-sm p-3 mb-5 bg-body rounded" style={{width:'25em'}}>
+                <h2  className="text-center shadow-sm p-3 mb-2 bg-body rounded">Astronomy Photo of the day</h2>
             <img src={spaces.hdurl}
-                className="card-img-top mt-2" alt="placeholder image"/>
+                className="card-img-top shadow-sm p-2 mb-1 bg-body rounded" alt="placeholder image"/>
             <div className="card-body">
                 <h5 className="card-title text-center">{spaces.title}</h5>
                 <p className="card-text text-center">{spaces.explanation}</p>
