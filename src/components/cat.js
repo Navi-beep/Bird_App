0c163dcf546f7f01e6d87003e83d68077a18ef26
Navi-Beep <estawars30@gmail.com>
2022-12-08 22:27:09 -0600

    made changes

--- src/components/cat.js
+++ src/components/cat.js
@@ -19,8 +19,8 @@ export default function Cat(){
     return (
 
         <>
-        <div className="card dflex container-fluid mt-3 shadow-sm p-3 mb-5 bg-body rounded" style={{width:'25em'}}>
-            <img src="http://placekitten.com/g/200/200" className="card-img-top mt-3" alt="..."/>
+        <div className="card dflex container-fluid mt-3 shadow-sm p-3 mb-5 bg-body" style={{width:'25em'}}>
+            <img src="http://placekitten.com/g/200/200" className="card-img-top shadow-sm p-2 mb-1 bg-body rounded" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title text-center">Random Cat Fact!</h5>
                 <p className="card-text">{cats.fact}</p>
